78e8c0497668dbded62e7839e26351831e225950
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-30 20:32:16 -0700

    Complete webpack.config.js file.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const {resolve} = require('path')
+const webpack = require('webpack')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpackValidator = require('webpack-validator')
+const {getIfUtils, removeEmpty} = require('webpack-config-utils')
+const OfflinePlugin = require('offline-plugin')
+
+
+
+module.exports = {
+  entry: "./app/index.js",
+  output: {
+    filename: "./public/bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'display-rainfall demo'
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+    }]
+  }
+
+}
