6a34eb6f918e7eb8786e13979589928c1f567127
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-31 16:01:44 -0700

    Restart.

--- app/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Router} from 'react-router'
-import routes from './app/config/routes'
-
-ReactDOM.render(
-  <Router>{routes}</Router>,
-  document.getElementById('app')
-)
